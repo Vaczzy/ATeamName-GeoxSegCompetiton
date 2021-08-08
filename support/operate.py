0c395b164a9a4238690ab56cd628820c35c1e1d8
Vaczzy <49486975+Vaczzy@users.noreply.github.com>
2021-08-08 10:26:33 +0800

    Update operate.py

--- support/operate.py
+++ support/operate.py
@@ -1,8 +1,112 @@
-f = open("./data/ModelList.txt")
-line = f.readline()
-ModelNum=int(line)
-for i in range(ModelNum):
-    line = f.readline()
-print('New Model Num is '+str(ModelNum))
-print('New Model :'+line)
-f.close()
+import os
+import numpy as np
+import time
+ 
+classlist=['background','farmland','city','village','water','forest','grass','road','others']
+
+def WriteData(filePath,Best):
+    """
+    将获取的最佳结果写入log中，最佳结果存储在Best中
+    """
+    localtime = time.strftime(r"%Y-%m-%d %H:%M:%S", time.localtime())
+     
+    f = open(filePath,'w')
+    f.write('Time: '+localtime+'\n')
+    f.write('BEST LIST\n')
+    f.write('* IoU: \n')
+    for i in range(9):
+        f.write(' '+classlist[i]+': '+str((Best['best_iou'])[i,0])\
+            +'   '+Best['best_iou_mark'][i]+'\n')
+    f.write('* mIoU: '+str(Best['best_miou'])+'   '+Best['best_miou_mark']+'\n')
+    f.write('* Acc: \n')
+    for i in range(9):
+        f.write(' '+classlist[i]+': '+str((Best['best_acc'])[i,0])\
+            +'   '+Best['best_acc_mark'][i]+'\n')
+    f.write('* mAcc: '+str(Best['best_macc'])+'   '+Best['best_macc_mark']+'\n')
+    f.write('* aAcc: '+str(Best['best_aacc'])+'   '+Best['best_aacc_mark']+'\n')
+    f.close()
+
+
+def ReadSelectBest(path):
+    """
+    读取存放在指定位置的提交结果，并寻找最佳结果
+    路径总为./data
+    """
+    # 获取之前的最佳结果
+    best_iou=np.zeros((9,1)) # 9种地物类型对应的最佳iou
+    best_iou_mark=['','','','','','','','','']
+    best_acc=np.zeros((9,1))
+    best_acc_mark=['','','','','','','','','']
+
+    best_aacc=0
+    best_aacc_mark=['']
+    best_miou=0
+    best_miou_mark=['']
+    best_macc=0
+    best_macc_mark=['']
+
+    
+    # 检查提交的结果
+    for folder in os.listdir(path):
+        f = open(path+'/'+folder+'/index.txt')
+        
+        modelName = ((f.readline()).split(':')[1]).split('\n')[0] 
+        submitter = ((f.readline()).split(':')[1]).split('\n')[0] 
+        time = ((f.readline()).split(':')[1]).split('\n')[0]
+        line = f.readline()
+        line = f.readline()
+        line = f.readline()
+        for i in range(9):
+            element=f.readline()
+            IoU = float(element.split('|')[2])
+            if IoU>best_iou[i,0]:
+                best_iou[i,0]=IoU
+                best_iou_mark[i]=modelName+', '+submitter+', '+time
+            Acc = float(element.split('|')[3])
+            if Acc>best_acc[i,0]:
+                best_acc[i,0]=Acc
+                best_acc_mark[i]=modelName+', '+submitter+', '+time
+        line = f.readline()
+        line = f.readline()
+        line = f.readline()
+        line = f.readline()
+
+        element=f.readline()
+        aAcc=float(element.split('|')[1])
+        if aAcc>best_aacc:
+            best_aacc=aAcc
+            best_aacc_mark=modelName+', '+submitter+', '+time
+        mIoU=float(element.split('|')[2])
+        if mIoU>best_miou:
+            best_miou=mIoU
+            best_miou_mark=modelName+', '+submitter+', '+time
+        mAcc=float(element.split('|')[3])
+        if mAcc>best_macc:
+            best_macc=mAcc
+            best_macc_mark=modelName+', '+submitter+', '+time
+        f.close()
+
+
+    # 字典
+    Best={}
+    Best['best_iou']=best_iou
+    Best['best_iou_mark']=best_iou_mark
+    Best['best_acc']=best_acc
+    Best['best_acc_mark']=best_acc_mark
+
+    Best['best_aacc']=best_aacc
+    Best['best_aacc_mark']=best_aacc_mark
+    Best['best_miou']=best_miou
+    Best['best_miou_mark']=best_miou_mark
+    Best['best_macc']=best_macc
+    Best['best_macc_mark']=best_macc_mark
+
+    return Best
+
+path='./data' # 保存提交结果的路径
+
+time_mark=time.strftime(r"%Y_%m_%d_%H_%M_%S", time.localtime())
+bestFolderName='./Best_'+time_mark+'1.log' # 保存当前最佳结果的路径
+
+Best=ReadSelectBest(path)
+WriteData(bestFolderName,Best)
