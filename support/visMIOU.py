4a9bd9133309b5898d6ba64ed688ece2e323a66f
Vaczzy <49486975+Vaczzy@users.noreply.github.com>
2021-08-12 12:45:52 +0800

    Create visMIOU.py

---
+++ support/visMIOU.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import time
+
+# read mIoU
+path='./Rank_mIoU/rank.log'
+f=open(path,'r')
+title='mIoU Rank '+f.readline()
+ModelNum=int((f.readline()).split(':')[1]).split('\n')[0])
+line=f.readline()
+name_list=[]
+num_list=[]
+for i in range(ModelNum):
+    element=f.readline().split(', ')[1]
+    name_list.append(element.split(':')[0])
+    num_list.append((element.split(':')[1]).split('\n')[0])
+# darw and save
+time_mark=time.strftime(r"%Y_%m_%d_%H_%M_%S", time.localtime())
+savepath='./visual/mIoUrank_'+time_mark+'.png'
+
+plt.title(title)
+plt.barh(range(ModelNum), num_list,tick_label=name_list)
+plt.savefig(savepath)
