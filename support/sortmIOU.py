9377f76a4def0608dec40dde88df5d2aa252b982
Vaczzy <49486975+Vaczzy@users.noreply.github.com>
2021-08-12 10:32:15 +0800

    Update sortmIOU.py

--- support/sortmIOU.py
+++ support/sortmIOU.py
@@ -1,6 +1,7 @@
 # 按照平均IoU排序
 import numpy as np
 import os
+
 def readIOU(path):
     mIoU={}
     i=0
@@ -24,9 +25,25 @@ def readIOU(path):
         f.close()
     return mIoU
 
+def WriteData(filePath,smIoU):
+    """
+    将获取的最佳结果写入log中，最佳结果存储在Best中
+    """
+    ts=time.time()
+    tz = pytz.timezone('Asia/Shanghai')
+    dt = pytz.datetime.datetime.fromtimestamp(ts, tz)
+    localtime=dt.strftime('%Y-%m-%d %H:%M:%S')
+    f = open(filePath,'w')
+    f.write('Time: '+localtime+'\n')
+    f.write('*Sort by mIoU: \n')
+    for i in len(smIoU):
+        f.write(str(i)+',  '+smIoU[i][0]+': '+smIoU[i][1])
+    f.close()
+    
 path='./data'
+filepath='./Rank_mIoU/rank.log'
 mIoU=readIoU(path)
 smIoU=sorted(mIoU.items(), key = lambda kv:(kv[1], kv[0]))
 # 写文件
-
+WriteData(filePath,smIoU)
 
