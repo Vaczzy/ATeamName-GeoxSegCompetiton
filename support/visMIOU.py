406a960329dff6515fec941b8f10ea42d97925a1
Vaczzy <49486975+Vaczzy@users.noreply.github.com>
2021-08-12 13:56:07 +0800

    Update visMIOU.py

--- support/visMIOU.py
+++ support/visMIOU.py
@@ -4,8 +4,9 @@ import time
 # read mIoU
 path='./Rank_mIoU/rank.log'
 f=open(path,'r')
-title='mIoU Rank '+f.readline()
-ModelNum=int(((f.readline()).split(':')[1]).split('\n')[0])
+title='mIoU Rank (Top 5)'+f.readline()
+#ModelNum=int(((f.readline()).split(':')[1]).split('\n')[0])
+ModelNum=5 # top 5
 line=f.readline()
 name_list=[]
 num_list=[]
@@ -13,6 +14,7 @@ for i in range(ModelNum):
     element=f.readline().split(', ')[1]
     name_list.append(element.split(':')[0])
     num_list.append(float((element.split(':')[1]).split('\n')[0]))
+    
 # darw and save
 time_mark=time.strftime(r"%Y_%m_%d_%H_%M_%S", time.localtime())
 savepath='./visual/mIoUrank_'+time_mark+'.png'
